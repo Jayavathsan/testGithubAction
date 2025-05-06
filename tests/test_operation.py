4daad78136ef9dc1730aec2b6789bf2f50aeae43
JJ <127033262+Jayavathsan@users.noreply.github.com>
2025-05-06 13:02:37 +0530

    Update test_operation.py
    rectified test logic

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -4,8 +4,10 @@ def test_add():
     assert add(2,5)==7
     assert add(-1,1)==0
     assert add(10,5)==15
+    assert add(10,-10)==0
 
 def test_sub():
     assert sub(5,3)==2
     assert sub(4,6)==-2
-    assert sub(5,-5)==0
+    assert sub(5,-5)==10
+    assert sub(5,5)=0
